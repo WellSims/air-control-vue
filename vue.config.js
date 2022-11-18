5c991f878255db3faef4b742016e447cf7763b63
Welington Simões <welin099@gmail.com>
2022-11-18 12:21:03 -0300

    Alteração de HTTP para HTTPS em devServer.

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,21 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 function resolveSrc(_path) {
   return path.join(__dirname, _path);
 }
 
 module.exports = {
+  devServer: {
+    https: true
+  },
   lintOnSave: false,
   configureWebpack: {
     // Set up all the aliases we use in our app.
     resolve: {
       alias: {
-        src: resolveSrc('src'),
-        'chart.js': 'chart.js/dist/Chart.js'
+        src: resolveSrc("src"),
+        "chart.js": "chart.js/dist/Chart.js"
       }
     },
     plugins: [
@@ -22,14 +25,14 @@ module.exports = {
     ]
   },
   pwa: {
-    name: 'Vue Light Bootstrap Dashboard',
-    themeColor: '#344675',
-    msTileColor: '#344675',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '#344675'
+    name: "Vue Light Bootstrap Dashboard",
+    themeColor: "#344675",
+    msTileColor: "#344675",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#344675"
   },
   css: {
     // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
+    sourceMap: process.env.NODE_ENV !== "production"
   }
 };
